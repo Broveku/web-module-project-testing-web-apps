9d2c4ac546db478ce294e880fa2e9b26b11dc5b1
Broveku <gearhearjordan@gmail.com>
2021-08-05 15:01:17 -0700

    finished writing tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -31,21 +31,61 @@ test('renders ONE error message if user enters less then 5 characters into first
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
     render(<ContactForm/>)
+    
+    const submitButton = screen.getByRole('button')
+    userEvent.click(submitButton)
+    
+    const inputErrors= screen.getAllByTestId(/error/)
+    expect(inputErrors[0]).toHaveTextContent('firstName must have at least 5 characters.')
+    expect(inputErrors[1]).toHaveTextContent('lastName is a required field.')
+    expect(inputErrors[2]).toHaveTextContent('email must be a valid email address.')
 
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
     render(<ContactForm/>)
 
+    const firstNameInput= screen.getByLabelText(/first name*/i)
+    userEvent.type(firstNameInput, 'Jordan')
+
+    const lastNameInput= screen.getByLabelText(/last name*/i)
+    userEvent.type(lastNameInput, 'Gearheart')
+
+    const emailInput= screen.getByLabelText(/email*/i)
+    userEvent.type(emailInput, 'badEmail@dotcom')
+
+    const emailError= screen.getByText(/email must be a valid email address./i)
+    expect(emailError).toBeInTheDocument()
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
     render(<ContactForm/>)
 
+    const emailInput= screen.getByLabelText(/email*/i)
+    userEvent.type(emailInput, 'a')
+
+    const emailError= screen.getByText(/email must be a valid email address./i)
+    expect(emailError).toBeInTheDocument()
+
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    render(<ContactForm/>)
+    render(<ContactForm/>) 
+
+    const firstNameInput= screen.getByLabelText(/first name*/i)
+    userEvent.type(firstNameInput, 'Jordan')
+
+    const lastNameInput = screen.getByLabelText(/last name*/i)
+    userEvent.type(lastNameInput, '')
+
+    const emailInput = screen.getByLabelText(/email*/i)
+    userEvent.type(emailInput, 'jordan@jordan.com')
+
+    const submitButton = screen.getByRole('button')
+    userEvent.click(submitButton)
+
+    const lastNameError = screen.getByText(/lastName is a required field/i)
+    expect(lastNameError).toBeInTheDocument()
 
 });
 
